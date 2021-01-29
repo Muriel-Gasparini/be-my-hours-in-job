02847dcc7f34386bdb1c9b5df24c2a7da8b56445
Muriel Gasparini <muriel.developer@gmail.com>
2021-01-28 22:28:31 -0300

    add name in the body validation

--- src/middlewares/body-validation/schemas/login.js
+++ src/middlewares/body-validation/schemas/login.js
@@ -1,6 +1,7 @@
 import Joi from 'joi'
 
 export default Joi.object({
+  name: Joi.string().min(3).required(),
   login: Joi.string().min(4).required(),
   password: Joi.string().min(5).required(),
   confirmPassword: Joi.string().min(5).required()
