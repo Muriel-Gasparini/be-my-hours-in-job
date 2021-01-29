8ee26074b2bfce530709f26662a6749613215fcf
Muriel Gasparini <muriel.developer@gmail.com>
2021-01-28 22:29:11 -0300

    create sign up controller

---
+++ src/controllers/signup.js
@@ -0,0 +1,18 @@
+import { UserRepository } from '../database/repositories/user-repository'
+import { validator } from '../middlewares/body-validation/validator'
+import Notify from '../utils/notify'
+
+
+export async function signUpController (req, res) {
+  try {
+    const { isError, message } = validator('login', req.body)
+
+    if (isError) return res.status(400).json(message)
+
+    const userAccount = await UserRepository.create(req.body)
+    
+    res.status(201).json(userAccount)
+  } catch (error) {
+    Notify.error('Error while sign up', error)
+  }
+}
